9cf15adacfc86a13424c66c9769bd84e56b92e24
김우섭 <azamara@prandy.biz>
2014-02-11 16:04:20 +0900

    update

--- mvc/cornerstone/template/template.js
+++ mvc/cornerstone/template/template.js
@@ -4,4 +4,4 @@
     COPYRIGHT(C) 2012 BY SKTELECOM CO., LTD. ALL RIGHTS RESERVED.
     Released under the Apache License, Version 2.0
 */
-define([".","handlebars"],function(a,b){return{load:function(c,d,e){a.ajax({url:d.toUrl(c+".template"),success:function(a){e(b.compile(a))},error:function(a,b,c){e.error(c)}})}}});
+define(["jquery","handlebars"],function(a,b){return{load:function(c,d,e){a.ajax({url:d.toUrl(c+".template"),success:function(a){e(b.compile(a))},error:function(a,b,c){e.error(c)}})}}});
